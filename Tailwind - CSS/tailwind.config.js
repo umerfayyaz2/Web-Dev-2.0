46bedef3146fd63a395aab3870064701095b5175
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-08-09 21:48:08 +0500

    deleted tailwind files

--- Tailwind - CSS/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./Lesson-1/build/*.html'], /* Added File Path/Address here */
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
