fb111a168eac1427a59dfd5285789a42c445c01d
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-08-09 21:25:40 +0500

    Introduction & Setting-Up Tailwind CSS

---
+++ Tailwind - CSS/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./Lesson-1/build/*.html'], /* Added File Path/Address here */
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
