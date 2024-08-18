f325701dcd8d24b2693dae2ae677adcfff5676df
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-08-18 15:05:12 +0500

    slight changes

---
+++ JavaScript - JS LABS/Day - 1/1.3 infinity_and_NaN.js
@@ -0,0 +1,5 @@
+Infinity                // Read/write variable initialized to Infinity.
+Number.POSITIVE_INFINITY // Same as Infinity, but read-only.
+Number.NEGATIVE_INFINITY // Represents negative infinity.
+NaN                     // Read/write variable initialized to NaN.
+Number.NaN              // Read-only property holding NaN value.
