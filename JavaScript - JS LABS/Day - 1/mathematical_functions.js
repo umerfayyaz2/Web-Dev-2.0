ec91df7617247b5f861da99c5c14b238ae5b6ce2
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-08-18 15:02:57 +0500

    slight changes

---
+++ JavaScript - JS LABS/Day - 1/mathematical_functions.js
@@ -0,0 +1,13 @@
+Math.pow(2,53)      // 9007199254740992: 2 to the power of 53
+Math.round(.6)      // 1.0: rounds to the nearest integer
+Math.ceil(.6)       // 1.0: rounds up to the nearest integer
+Math.floor(.6)      // 0.0: rounds down to the nearest integer
+Math.abs(-5)        // 5: absolute value
+Math.max(x,y,z)     // Returns the largest argument
+Math.min(x,y,z)     // Returns the smallest argument
+Math.random()       // Pseudo-random number between 0 (inclusive) and 1.0 (exclusive)
+Math.PI             // π (circumference of a circle / diameter)
+Math.E              // Euler's number (base of the natural logarithm)
+Math.sqrt(3)        // Square root of 3
+Math.sin(0)         // Sine of 0 (also Math.cos, Math.atan, etc.)
+Math.log(10)        // Natural logarithm of 10
