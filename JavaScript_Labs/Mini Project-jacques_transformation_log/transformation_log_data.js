74ef4f6d60ec99563ce5430ba0502d181c7f6870
umerfayyaz2 <ufayyaz19@gmail.com>
2025-02-03 00:23:50 +0500

    This file defines the initial data structure for Jacques’s log.

---
+++ JavaScript_Labs/Mini Project-jacques_transformation_log/transformation_log_data.js
@@ -0,0 +1,20 @@
+// transformation_log_data.js
+const transformationLog = [
+    {
+      date: "2023-10-01",
+      activities: ["worked", "walked near oak trees", "ate walnuts"],
+      transformed: true,
+    },
+    {
+      date: "2023-10-02",
+      activities: ["read a book", "avoided oak trees", "watched TV"],
+      transformed: false,
+    },
+    {
+      date: "2023-10-03",
+      activities: ["gardened", "climbed a tree", "ate pizza"],
+      transformed: true,
+    },
+  ];
+  
+  module.exports = transformationLog;
