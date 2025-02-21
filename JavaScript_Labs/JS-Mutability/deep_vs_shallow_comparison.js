4217b02198d9065698dafaa8299110b6432bf149
umerfayyaz2 <ufayyaz19@gmail.com>
2025-02-21 13:32:40 +0500

    added deep_vs_shallow_comparison.js

---
+++ JavaScript_Labs/JS-Mutability/deep_vs_shallow_comparison.js
@@ -0,0 +1,25 @@
+function deepEqual(obj1, obj2) {
+    if (obj1 === obj2) return true; 
+    if (typeof obj1 !== "object" || obj1 === null ||
+        typeof obj2 !== "object" || obj2 === null) {
+      return false; 
+    }
+  
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
+  
+    if (keys1.length !== keys2.length) return false; 
+  
+    for (let key of keys1) {
+      if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+        return false; 
+      }
+    }
+  
+    return true; 
+  }
+  
+  let obj1 = { a: { b: 2 } };
+  let obj2 = { a: { b: 2 } };
+  
+  console.log(deepEqual(obj1, obj2));
