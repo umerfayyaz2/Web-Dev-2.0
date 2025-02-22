694209afbacafeb519cb9937a28f0ff6e8b6ba2b
umerfayyaz2 <ufayyaz19@gmail.com>
2025-02-22 15:12:04 +0500

    Added 1_array_destructuring_example.js

---
+++ JavaScript_Labs/JS-Destructuring/1_array_destructuring_example.js
@@ -0,0 +1,7 @@
+let table = [1, 9, 4, 76];
+let [n00, n01, n10, n11] = table;
+
+console.log("n00:", n00); // Output: 1
+console.log("n01:", n01); // Output: 9
+console.log("n10:", n10); // Output: 4
+console.log("n11:", n11); // Output: 76
