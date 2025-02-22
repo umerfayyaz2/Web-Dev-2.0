927f4c49ba3aa56c1ae135f50e5a2bf68051c1f4
umerfayyaz2 <ufayyaz19@gmail.com>
2025-02-22 15:13:22 +0500

    Added 3_default_values_example.js

---
+++ JavaScript_Labs/JS-Destructuring/3_default_values_example.js
@@ -0,0 +1,7 @@
+// Array example
+let [a, b = 2] = [1];
+console.log("a:", a, "b:", b); // Output: 1 2
+
+// Object example
+let { x = 10, y } = { y: 5 };
+console.log("x:", x, "y:", y); // Output: 10 5
