314f5bf155344c9ba53588f445a31ece8a62e9f9
umerfayyaz2 <ufayyaz19@gmail.com>
2025-02-24 22:26:42 +0500

    Added static_methods.js

---
+++ JavaScript_Labs/JS-Object Oriented Programming/4_Classes/static_methods.js
@@ -0,0 +1,12 @@
+class MathUtils {
+    static add(a, b) {
+        return a + b;
+    }
+
+    static multiply(a, b) {
+        return a * b;
+    }
+}
+
+console.log(MathUtils.add(5, 3));       // Output: 8
+console.log(MathUtils.multiply(4, 6));  // Output: 24
