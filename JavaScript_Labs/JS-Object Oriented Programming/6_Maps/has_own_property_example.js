c75af676826d108f035f80564a5d2320e000116f
umerfayyaz2 <ufayyaz19@gmail.com>
2025-02-26 02:39:48 +0500

    Added has_own_property_example.js

--- JavaScript_Labs/JS-Object Oriented Programming/6_Maps/has_own_property_example.js
+++ JavaScript_Labs/JS-Object Oriented Programming/6_Maps/has_own_property_example.js
@@ -2,4 +2,4 @@ let obj = { x: 1 };
 
 // Using hasOwnProperty
 console.log(obj.hasOwnProperty("x")); // Output: true
-console.log(obj.hasOwnProperty("toString")); // Output: false
+console.log(obj.hasOwnProperty("toString")); // Output: false 
