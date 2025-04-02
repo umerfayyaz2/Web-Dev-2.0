528371a8e768ca18c0ec3773c713df3b070135d6
umerfayyaz2 <ufayyaz19@gmail.com>
2025-04-02 11:03:12 +0500

    Add and export math functions (add, subtract, multiply, divide)

---
+++ JavaScript_Labs/JS-Modules/math_operations.js
@@ -0,0 +1,17 @@
+// Exporting multiple functions using named exports
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export function multiply(a, b) {
+    return a * b;
+}
+
+export function divide(a, b) {
+    if (b === 0) throw new Error("Division by zero is not allowed.");
+    return a / b;
+}
