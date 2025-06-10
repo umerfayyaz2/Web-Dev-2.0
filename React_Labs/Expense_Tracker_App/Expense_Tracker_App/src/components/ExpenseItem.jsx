fd8c801b7e4300af00f8d68fffb20b793bf76f57
umerfayyaz2 <ufayyaz19@gmail.com>
2025-06-10 17:13:48 +0500

    Added Dynamic placeholders for data in ExpenseItem file

--- React_Labs/Expense_Tracker_App/Expense_Tracker_App/src/components/ExpenseItem.jsx
+++ React_Labs/Expense_Tracker_App/Expense_Tracker_App/src/components/ExpenseItem.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./ExpenseItem.css";
 const ExpenseItem = () => {
+  const expenseDate = new Date(2025, 1, 1);
+  const expenseTitle = "Car Parking";
+  const expenseAmount = 12;
+
   return (
     <div className="expense-item">
-      <div>16 March 2025</div>
+      <div>{expenseDate.toISOString()}</div>
 
       <div className="expense-item__description">
-        <h2>Car Parking</h2>
-        <div className="expense-item__price">$12</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
