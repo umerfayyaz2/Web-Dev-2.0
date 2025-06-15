8dc364e08c5ffe60896b8289e1e42da059c88813
umerfayyaz2 <ufayyaz19@gmail.com>
2025-06-15 21:52:07 +0500

    finalized props and expense item component

--- React_Labs/Expense_Tracker_App/Expense_Tracker_App/src/components/ExpenseItem.jsx
+++ React_Labs/Expense_Tracker_App/Expense_Tracker_App/src/components/ExpenseItem.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./ExpenseItem.css";
-const ExpenseItem = () => {
-  const expenseDate = new Date(2025, 1, 1);
-  const expenseTitle = "Car Parking";
-  const expenseAmount = 12;
-
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
 
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
